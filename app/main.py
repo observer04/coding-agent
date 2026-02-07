914ca9c1527b4adb0291e7085b7f90479cca1fd6
observer04 <tripathysarojinee@gmail.com>
2026-02-08 01:51:54 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -19,8 +19,7 @@ def main():
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
     chat = client.chat.completions.create(
- #       model="anthropic/claude-haiku-4.5",
-	model="z-ai/glm-4.5-air:free",
+        model="anthropic/claude-haiku-4.5",
         messages=[{"role": "user", "content": args.p}],
 	tools=[{
   "type": "function",
